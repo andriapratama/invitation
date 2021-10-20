170a9285a5be9e330632f03c30edb922694f9cc3
andrelocong <locong@Macbooks-MacBook-Air.local>
2021-10-20 13:35:16 +0800

    create feature

--- src/client/home/Home.jsx
+++ src/client/home/Home.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import './Home.css';
 import Hero from "./components/hero/Hero";
+import Feature from "./components/feature/Feature";
 
 const Home = () => {
     return (
         <div className="home">
             <Hero />
-            <div className="description"></div>
-            <div className="feature"></div>
+            <div className="description">
+                <h1>Hello</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quae nostrum iure? Praesentium facere nemo beatae eum ex autem mollitia eos quos, reiciendis nulla impedit inventore odit animi sint voluptas!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis soluta natus quam tempore eos, voluptatum error debitis ad voluptas temporibus repellendus sequi sit molestias, porro, quia reprehenderit. Distinctio, ipsa praesentium.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui molestiae vel consequatur eius vitae cum veritatis culpa aperiam incidunt magnam. Voluptas cum voluptate, earum dolorem ipsum eum debitis laudantium unde?
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure quis possimus temporibus odio alias ea, mollitia perferendis non, eaque veniam corrupti ad dolores, nesciunt illo natus voluptate blanditiis repellat quos!
+                </p>
+            </div>
+            <Feature />
             <div className="feedback"></div>
             <div className="footer"></div>
         </div>
---
+++ src/client/home/components/feature/Feature.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './Feature.css';
+
+const Feature = () => {
+    return (
+        <div className="feature">
+            <h1>Feature we have</h1>
+            <div className="feature-body">
+                <div className="feature-box"></div>
+                <div className="feature-box"></div>
+                <div className="feature-box"></div>
+                <div className="feature-box"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Feature;
