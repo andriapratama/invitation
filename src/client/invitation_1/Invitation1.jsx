b62f06acd094e6f8294acee6688526cc12c31453
andrelocong <locong@Macbooks-MacBook-Air.local>
2021-10-21 01:08:15 +0800

    create invitation1

--- src/client/invitation_1/Invitation1.jsx
+++ src/client/invitation_1/Invitation1.jsx
@@ -1,21 +1,39 @@
 import React from "react";
 import './Invitation1.css';
+import Protokol from "./protokol/Protokol";
 
 const Invitation1 = () => {
     return (
         <div className="invitation1">
             <div className="cover">
                 <p>we invite you to our ceremony</p>
-                <p>Upacara Manusa Yadnya</p>
+                <p className="upacara">Upacara Manusa Yadnya</p>
                 <h1>Mepandes</h1>
                 <h2>Keluarga Besar Putu Gede Wayan</h2>
-                <p>11 November 2021</p>
+                <p className="dates">11 November 2021</p>
                 <p>Mengundang Bapak/Ibu/Saudara/i</p>
-                <h1>Yuda</h1>
+                <h3>Yuda</h3>
             </div>
-            <div className="nama"></div>
-            <div className="detail-event"></div>
-            <div className="protokol"></div>
+            <div className="nama">
+                <h1 className="title">Om Swastiyastu</h1>
+                <p>Atas Asung Kerta Waru Nugraha Ida Sang Hyang Widhi Wasa, kami bermaksud untuk mengundang bapak/ibu/saudara/i ke acara manusa yadnya Mepandes (potong gigi) putra-putri kami :</p>
+                <h2>Ajeng Pradnyaswari Suryawirawan</h2>
+                <h2>I Putu Aditya Widharma Satya</h2>
+                <h2>Putu Etha Naraswari Wira </h2>
+                <h2>Dharma Pradnyananda Suryawirawan</h2>
+                <h2>Ni Made Arindra Widyaswari</h2>
+                <h2>Putu Satya Keshawa</h2>
+                <h2>Made Endhita Candraswari</h2>
+                <h2>Made Sadwika Mahindra Dinata</h2>
+            </div>
+            <div className="detail-event">
+                <h1 className="title">Waktu dan Tempat</h1>
+                <h3>11 November 2021</h3>
+                <p>Jumat, 09.00-18.00</p>
+                <p>Jalan Desa Blahbatuh, Gianyar</p>
+                <button>Buka Map</button>
+            </div>
+            <Protokol />
             <div className="footer"></div>
         </div>
     )
---
+++ src/client/invitation_1/protokol/Protokol.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import './Protokol.css';
+import Icon1 from '../../../icons/avoid-crowds.png';
+import Icon2 from '../../../icons/handshake.png';
+import Icon3 from '../../../icons/mask.png';
+import Icon4 from '../../../icons/thermometer.png';
+import Icon5 from '../../../icons/washing-hands.png';
+
+const Protokol = () => {
+    return (
+        <div className="protokol">
+            <h1 className="title">Protokol Kesehatan</h1>
+            <p>Acara diselenggarakan dengan protokol kesehatan yang harus dipatuhi demi mencegah penyebaran virus Covid-19. Tanpa mengurangi rasa hormat, tamu undangan diharapkan untuk mengikuti protokol kesehatan.</p>
+            <div className="protokol-body">
+                <div className="protokol-group">
+                    <img src={Icon1} alt="Jaga jarak" />
+                    <p>Jaga jarak & hindari kerumunan</p>
+                </div>
+                <div className="protokol-group">
+                    <img src={Icon3} alt="Memakai masker" />
+                    <p>Wajib masker</p>
+                </div>
+                <div className="protokol-group">
+                    <img src={Icon4} alt="Cek suhu tubuh" />
+                    <p>Cek suhu tubuh</p>
+                </div>
+                <div className="protokol-group">
+                    <img src={Icon5} alt="Mencuci tangan" />
+                    <p>Mencuci tangan</p>
+                </div>
+                <div className="protokol-group">
+                    <img src={Icon2} alt="Tidak berjabat tangan" />
+                    <p>Tidak berjabat tangan</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Protokol;
